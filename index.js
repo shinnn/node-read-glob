621dac68886ef86963720eeac6b49c6c33030b8d
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-12-19 10:40:57 +0900

    update dependencies and devDependencies
    Highlights:
    
    * xtend -> object-assign
    * istanbul -> nyc
    * istanbul-coveralls -> coveralls
    * update ESLint to v4
    * introduce unglobbable
    * introduce @shinnn/eslint-config and follow its coding rules

--- index.js
+++ index.js
@@ -2,44 +2,42 @@
 
 var assertFsReadFileOption = require('assert-fs-readfile-option');
 var glob = require('glob');
+var objectAssign = require('object-assign');
 var readMultipleFiles = require('read-multiple-files');
-var xtend = require('xtend');
 
 var defaultOption = {nodir: true};
 
 module.exports = function readGlob(globPattern, options, cb) {
-  if (cb === undefined) {
-    cb = options;
-    options = defaultOption;
-  } else {
-    if (options) {
-      assertFsReadFileOption(options);
-      if (typeof options === 'string') {
-        options = {
-          encoding: options,
-          nodir: true
-        };
-      } else {
-        options = xtend(defaultOption, options);
-      }
-    } else {
-      options = defaultOption;
-    }
-  }
+	if (cb === undefined) {
+		cb = options;
+		options = defaultOption;
+	} else if (options) {
+		assertFsReadFileOption(options);
+		if (typeof options === 'string') {
+			options = {
+				encoding: options,
+				nodir: true
+			};
+		} else {
+			options = objectAssign({nodir: true}, options);
+		}
+	} else {
+		options = defaultOption;
+	}
 
-  if (typeof cb !== 'function') {
-    throw new TypeError(cb + ' is not a function. Last argument must be a function.');
-  }
+	if (typeof cb !== 'function') {
+		throw new TypeError(cb + ' is not a function. Last argument must be a function.');
+	}
 
-  var g = new glob.Glob(globPattern, options, function(err, filePaths) {
-    if (err) {
-      g.abort();
-      cb(err);
-      return;
-    }
+	var g = new glob.Glob(globPattern, options, function(err, filePaths) {
+		if (err) {
+			g.abort();
+			cb(err);
+			return;
+		}
 
-    readMultipleFiles(filePaths, options, cb);
-  });
+		readMultipleFiles(filePaths, options, cb);
+	});
 
-  return g;
+	return g;
 };
--- test.js
+++ test.js
@@ -1,90 +1,90 @@
 'use strict';
 
-var glob = require('glob');
-var readGlob = require('./');
-var test = require('tape');
-var xtend = require('xtend');
+const glob = require('glob');
+const readGlob = require('.');
+const test = require('tape');
+const unglobbable = require('unglobbable');
 
-test('readGlob()', function(t) {
-  t.plan(13);
+test('readGlob()', t => {
+	t.plan(13);
 
-  readGlob('{.git{attributes,ignore},node_*/{glob,xtend}}', 'utf8', function(err, contents) {
-    t.deepEqual(
-      [err, contents],
-      [null, ['* text=auto\n', 'coverage\nnode_modules\n']],
-      'should reflect encoding to the result.'
-    );
-  });
+	readGlob('{.git{attributes,ignore},node_*/{glob,lodash}}', 'utf8', (err, contents) => {
+		t.deepEqual(
+			[err, contents],
+			[null, ['* text=auto\n', '.nyc_output\ncoverage\nnode_modules\n']],
+			'should reflect encoding to the result.'
+		);
+	});
 
-  var options = {
-    nounique: true,
-    ignore: '.gitignore',
-    noglobstar: true,
-    encoding: 'hex'
-  };
+	const options = {
+		nounique: true,
+		ignore: '.gitignore',
+		noglobstar: true,
+		encoding: 'hex'
+	};
 
-  var optionsClone = xtend(options);
+	const optionsClone = Object.assign({}, options);
 
-  readGlob('{.*it*e{,s,s},**/test.js,node_*,../}', options, function(err, contents) {
-    var expected = new Buffer('* text=auto\n').toString('hex');
+	readGlob('{.*it*e{,s,s},**/test.js,node_*,../}', options, (err, contents) => {
+		const expected = Buffer.from('* text=auto\n').toString('hex');
 
-    t.deepEqual(
-      [err, contents],
-      [null, [expected, expected]],
-      'should reflect minimatch, node-glob and fs.readFile options to the result.'
-    );
-    t.deepEqual(options, optionsClone, 'should not modify the original option object.');
-  });
+		t.deepEqual(
+			[err, contents],
+			[null, [expected, expected]],
+			'should reflect minimatch, node-glob and fs.readFile options to the result.'
+		);
+		t.deepEqual(options, optionsClone, 'should not modify the original option object.');
+	});
 
-  readGlob('__foo__bar__baz__qux__', null, function(err, bufs) {
-    t.deepEqual(
-      [err, bufs],
-      [null, []],
-      'should pass an empty array to the callback when it reads no files.'
-    );
-  });
+	readGlob('__foo__bar__baz__qux__', null, (err, bufs) => {
+		t.deepEqual(
+			[err, bufs],
+			[null, []],
+			'should pass an empty array to the callback when it reads no files.'
+		);
+	});
 
-  readGlob('node_modules', {nodir: false}, function(err) {
-    t.equal(err.code, 'EISDIR', 'should fail when it cannot read the target.');
-  });
+	readGlob('node_modules', {nodir: false}, err => {
+		t.equal(err.code, 'EISDIR', 'should fail when it cannot read the target.');
+	});
 
-  var g = readGlob('/**/*', function(err) {
-    t.ok(g.aborted, 'should abort glob before it calls callback function.');
-    t.ok(
-      err.code,
-      'should fail when it tries to access unaccessible paths, such as ' + err.path
-    );
-  });
+	const g = readGlob(unglobbable, err => {
+		t.ok(g.aborted, 'should abort glob before it calls callback function.');
+		t.ok(
+			err.code,
+			`should fail when it tries to access unaccessible paths, such as ${err.path}`
+		);
+	});
 
-  t.strictEqual(g.constructor, glob.Glob, 'should return glob instance.');
+	t.equal(g.constructor, glob.Glob, 'should return glob instance.');
 
-  t.throws(
-    readGlob.bind(null, [''], t.fail),
-    /TypeError.*string required/,
-    'should throw a type error when the first argument is not a string.'
-  );
+	t.throws(
+		() => readGlob([''], t.fail),
+		/TypeError.*string required/,
+		'should throw a type error when the first argument is not a string.'
+	);
 
-  t.throws(
-    readGlob.bind(null, '*', 1, t.fail),
-    /TypeError.*argument/,
-    'should throw a type error when it takes invalid option value.'
-  );
+	t.throws(
+		() => readGlob('*', 1, t.fail),
+		/TypeError.*argument/,
+		'should throw a type error when it takes invalid option value.'
+	);
 
-  t.throws(
-    readGlob.bind(null, '', 'utf7', t.fail),
-    /Error.*encoding/,
-    'should throw a type error when the encoding is unknown.'
-  );
+	t.throws(
+		() => readGlob('', 'utf7', t.fail),
+		/Error.*encoding/,
+		'should throw a type error when the encoding is unknown.'
+	);
 
-  t.throws(
-    readGlob.bind(null, [''], true),
-    /TypeError.*Last argument/,
-    'should throw a type error when the last argument is not a function.'
-  );
+	t.throws(
+		() => readGlob([''], true),
+		/TypeError.*Last argument/,
+		'should throw a type error when the last argument is not a function.'
+	);
 
-  t.throws(
-    readGlob.bind(null),
-    /TypeError.*Last argument/,
-    'should throw a type error when it takes no arguments.'
-  );
+	t.throws(
+		() => readGlob(),
+		/TypeError.*Last argument/,
+		'should throw a type error when it takes no arguments.'
+	);
 });
